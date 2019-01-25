b2a871efcb8c4232f0b470a70242cb93febc2644
Liam-Rougoor <liam.rougoor@hotmail.nl>
2019-01-26 00:17:05 +0100

    Dialogue system

---
+++ Unity/Assets/Dialogue/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "NPC/Dialogue")]
+public class Dialogue : ScriptableObject //ScriptableObject or Serializable?
+{
+    public string[] sentences;
+
+    //The variables below will be replaced by a reference to the NPC
+    public string name;
+    public Sprite npcImage;
+}
---
+++ Unity/Assets/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField]
+    Dialogue dialogue;
+
+    DialogueManager dialogueManager;
+
+
+
+    private void Start()
+    {
+        dialogueManager = GameObject.Find("DialogueManager").GetComponent<DialogueManager>(); //Switch to singleton later
+    }
+
+    public void TriggerDialogue()
+    {
+        dialogueManager.StartConversation(dialogue);
+    }
+}
