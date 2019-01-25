29e56f87cb5dbdc20c1ee7591ab31a00f7dcc3c6
Charfire <charfiremaster@gmail.com>
2019-01-25 22:27:52 +0100

    Create moving player

---
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 2.5f;
+    private Transform playerLocation;
+    
+    void Start()
+    {
+        playerLocation = this.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal") * Time.deltaTime * movementSpeed;
+        float yMovement = Input.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed;
+
+        playerLocation.position = new Vector3(  playerLocation.position.x + xMovement,
+                                                playerLocation.position.y + yMovement,
+                                                playerLocation.position.z);
+    }
+}
