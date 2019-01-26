deea7cfb513cbcbc49edb405c97b75a33f0ebf62
Charfire <charfiremaster@gmail.com>
2019-01-26 09:38:45 +0100

    Follow player

---
+++ Unity/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Transform cameraLocation;
+    void Start()
+    {
+        cameraLocation = this.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Transform player = GameObject.FindWithTag("Player").transform;
+        Follow(player);
+    }
+
+    private void Follow(Transform player)
+    {
+        Vector3 newLocation = new Vector3(player.position.x,
+                                            player.position.y,
+                                            cameraLocation.position.z);
+        this.transform.position = newLocation;
+    }
+}
