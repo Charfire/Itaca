1b3669eac6ba85017ae335de3dda8931d5e96949
Charfire <charfiremaster@gmail.com>
2019-01-27 14:40:30 +0100

    Dingen

---
+++ Unity/Assets/Dialogue/SceneEnding.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "NPC/Dialogue/Ending/Scene Ending")]
+public class SceneEnding : DialogueEnding
+{
+    public int newScene;
+
+    public override bool EndDialogue(GameObject obj)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        return true;
+    }
+}
