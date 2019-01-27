c0a05bb9700979243bc7fa68c5aca1415f4b1a79
Unknown <Santurechia@gmail.com>
2019-01-27 02:04:56 +0100

    stones can now be mined
    player needs pickaxe manually enabled in inspector

---
+++ Unity/Assets/Scripts/Stone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    private int stoneAmount;
+    //private ParticleSystem particles;
+    
+    void Start()
+    {
+        stoneAmount = 5;
+        //particles = gameObject.GetComponent<ParticleSystem>();
+    }
+
+
+    public void pickStone(int amount)
+    {
+        stoneAmount -= amount;
+
+        //particles.Play();
+
+        if (stoneAmount <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
