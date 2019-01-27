64f47a5ad643a176c16828672a0f86da1d544b17
Charfire <charfiremaster@gmail.com>
2019-01-27 11:05:53 +0100

    Introduction scene

--- Unity/Assets/IntroductionScene.cs
+++ Unity/Assets/IntroductionScene.cs
@@ -5,16 +5,38 @@ using UnityEngine.UI;
 
 public class IntroductionScene : MonoBehaviour
 {
+    [SerializeField]
     private List<Sprite> cutscenes;
     private Image panel;
     private Canvas canvas;
+    [SerializeField]
     private GameObject fadeIn;
-
-    // Start is called before the first frame update
-    void Start()
+    private Queue<Sprite> cutsceneQueue;
+    
+    private void Awake()
     {
+        panel = GetComponent<Image>();
+        cutsceneQueue = new Queue<Sprite>();
         canvas = FindObjectOfType<Canvas>();
         Instantiate(fadeIn, canvas.transform);
+        foreach (Sprite scene in cutscenes)
+        {
+            cutsceneQueue.Enqueue(scene);
+        }
+        panel.sprite = cutsceneQueue.Dequeue();
     }
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (cutsceneQueue.Count == 0)
+            {
+                Debug.Log("Next scene");
+            } else
+            {
+                panel.sprite = cutsceneQueue.Dequeue();
+            }
+        }
+    }
 }
